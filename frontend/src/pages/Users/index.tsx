3a31f8ec83df3af39d77d80bbbfe497a13d8b909
renatomak <rento.mark.silva@gmail.com>
2021-10-29 16:42:31 -0300

    grupo de botoes para o CRUD

--- frontend/src/pages/Users/index.tsx
+++ frontend/src/pages/Users/index.tsx
@@ -1,12 +1,30 @@
+import { Button, Stack } from "@material-ui/core";
 import * as React from "react";
+import { Link } from "react-router-dom";
+import { Container } from "./styled";
 import UserTable from "./UserTable";
 
 export default function FormPropsTextFields() {
-  
   return (
-    <>
+    <Container>
+      <h1>Dados dos Clientes</h1>
       <UserTable />
-      <a href="/users/add"><button>Adicionar Usuário</button></a>
-    </>
+      <br />
+
+      <Stack direction="row" spacing={2}>
+        <Button variant="contained" color="success" href="/users/add">
+          Adicionar Usuário
+        </Button>
+        <Button variant="contained" color="warning" href="/users/add">
+          Visualizar
+        </Button>
+        <Button variant="contained" color="primary" href="/users/add">
+          Atualizar
+        </Button>
+        <Button variant="contained" color="error" href="/users/add">
+          Excluir
+        </Button>
+      </Stack>
+    </Container>
   );
 }
---
+++ frontend/src/pages/Users/styled.tsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    color: var(--white);
+    
+    h1 {
+        font-weight: 400;
+        text-shadow: 0px 1px 1px rgb(0 0 0 / 40%);
+        padding: 0;
+        font-size: 64px;
+    }
+`;
+export const Div = styled.div``;
