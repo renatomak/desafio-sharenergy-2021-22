96601fa196b8fdfc3ba0a6efe0f622fdc24df0c2
renatomak <rento.mark.silva@gmail.com>
2021-10-29 15:37:42 -0300

    table

---
+++ frontend/src/pages/Users/UserTable.tsx
@@ -0,0 +1,109 @@
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TablePagination,
+  TableRow,
+} from "@material-ui/core";
+import * as React from "react";
+import dadosClientes from "store/dadosClientes.json";
+
+interface Column {
+  id: "name" | "code" | "usinas";
+  label: string;
+  minWidth?: number;
+  align?: "right";
+  format?: (value: number) => string;
+}
+
+const columns: readonly Column[] = [
+  { id: "code", label: "Número", minWidth: 100 },
+  { id: "name", label: "Name", minWidth: 170 },
+  { id: "usinas", label: "Códigos das Usinas", minWidth: 170 },
+];
+
+interface Data {
+  code: string;
+  name: string;
+  usinas: Array<number>;
+}
+
+function createData(code: string, name: string, usinas:  Array<number> = []): Data {
+  return { name, code, usinas };
+}
+
+const rows = dadosClientes.map(({numeroCliente, nomeCliente, usinas}) => {
+  const arrayUsinas = usinas.map((elem) => elem.usinaId);
+  return createData(String(numeroCliente), nomeCliente, arrayUsinas);
+})
+
+
+export default function StickyHeadTable() {
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+  const handleChangePage = (event: unknown, newPage: number) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
+  return (
+    <Paper sx={{ width: "100%", overflow: "hidden" }}>
+      <TableContainer sx={{ maxHeight: 440 }}>
+        <Table stickyHeader aria-label="sticky table">
+          <TableHead>
+            <TableRow>
+              {columns.map((column) => (
+                <TableCell
+                  key={column.id}
+                  align={column.align}
+                  style={{ minWidth: column.minWidth }}
+                >
+                  {column.label}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((row) => {
+                return (
+                  <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                    {columns.map((column) => {
+                      const value = row[column.id];
+                      return (
+                        <TableCell key={column.id} align={column.align}>
+                          {column.format && typeof value === "number"
+                            ? column.format(value)
+                            : value}
+                        </TableCell>
+                      );
+                    })}
+                  </TableRow>
+                );
+              })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[10, 25, 100]}
+        component="div"
+        count={rows.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </Paper>
+  );
+}
