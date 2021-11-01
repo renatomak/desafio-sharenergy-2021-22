8046cb301e13b99ef22190d02efba3492d62182f
renatomak <rento.mark.silva@gmail.com>
2021-10-31 21:45:27 -0300

    refatoraçao

--- frontend/src/pages/Customers/FinancialStatement/styled.tsx
+++ frontend/src/pages/Customers/FinancialStatement/styled.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
+  margin: 0 auto;
 
   h1 {
     text-align: center;
--- frontend/src/pages/DataAnalysis/styled.tsx
+++ frontend/src/pages/DataAnalysis/styled.tsx
@@ -1,16 +1,12 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  background: linear-gradient(
-    to right,
-    rgba(236, 229, 221, 0.8) 0%,
-    rgba(0, 162, 162, 0.8) 100%
-  );
   height: 100vh;
   display: flex;
   flex-direction: column;
   text-align: center;
   color: var(--white);
+  margin: 0 auto;
 `;
 
 export const Div = styled.div``;
